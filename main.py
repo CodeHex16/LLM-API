abfd5e7302d42ca2970c4fdea1b1ca4389caf32e
bazz333 <matteobazzan333@gmail.com>
2025-03-05 14:53:30 +0100

    dockerized

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-os.system('pip install -r requirements.txt')
-
-from app import app 
+from app import app
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="0.0.0.0", port=5100, reload=True)
